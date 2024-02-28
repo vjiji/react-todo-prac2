b5977966a4bd2abcac041ee6ecbf2373281ee5f6
vjiji <restmi0711@gmail.com>
2024-02-28 13:16:25 +0900

    refactor: TodoDetail컴포넌트의 뷰와 관련된 코드를 컴포넌트 안에서 확인할 수 있도록 코드를 수정한다

--- src/pages/TodoDetail/useTodoDetail.jsx
+++ src/pages/TodoDetail/useTodoDetail.jsx
@@ -1,17 +1,12 @@
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const useTodoDetail = () => {
+export default function useTodoDetail() {
   const { todos } = useSelector((state) => state.todos);
-  const currentId = useParams().id;
   const navigate = useNavigate();
-
-  const todo = todos.filter(({ id }) => id === currentId)[0];
   const handleButtonClick = () => {
     navigate("/");
   };
 
-  return { todo, handleButtonClick };
-};
-
-export default useTodoDetail;
+  return { todos, handleButtonClick };
+}
